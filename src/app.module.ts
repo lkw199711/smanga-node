acb12b67823baa34bd1e2cdc7029bd0765a9b3f7
lkw199711 <lkw199711@163.com>
2024-05-25 19:25:14 +0800

    fix: 本地修正.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: 梁楷文 lkw199711@163.com
  * @Date: 2024-04-24 15:24:38
- * @LastEditors: 梁楷文 lkw199711@163.com
- * @LastEditTime: 2024-05-04 09:16:54
+ * @LastEditors: lkw199711 lkw199711@163.com
+ * @LastEditTime: 2024-05-25 19:24:43
  * @FilePath: \smanga-node\src\app.module.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
